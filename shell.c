1d9ada945f021184eb9b938e5cb963435501b22b
LocrianGoose <azairLocr@protonmail.ch>
2018-10-21 22:50:57 +0600

    stage2

--- shell.c
+++ shell.c
@@ -1,6 +1,11 @@
-// SPDX-License-Identifier: NOLICENSE AHAAHAH
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
 
 char *getWord(char *lastCh)
 {
@@ -10,31 +15,80 @@ char *getWord(char *lastCh)
 	int i = 0;
 
 	while ((buf = getchar()) != ' ' && buf != '\n') {
-		if (i >= maxLen)
-			word = realloc(word, (maxLen <<= 1) * sizeof(char));
+		if (i + 2 > maxLen) {
+			maxLen <<= 1;
+			word = realloc(word, maxLen * sizeof(char));
+		}
 		word[i++] = buf;
 	}
-	*lastCh = buf;
+	if (i == 0) {
+		*lastCh = 0;
+		free(word);
+		return NULL;
+	}
 	word[i] = 0;
+	*lastCh = buf;
 	return word;
 }
 
-char **get_list(void)
+void freeList(char **command)
 {
-	int maxLen = 4, i = 0;
+	for (int i = 0; command[i] != 0; i++) {
+		free(command[i]);
+	}
+	free(command);
+}
+
+char **getList(void)
+{
+	int maxLen = 2, i = 0;
 	char lastCh = ' ';
 	char **list = malloc(maxLen * sizeof(char *));
-
 	while (lastCh != '\n') {
-		if (i >= maxLen)
-			list = realloc(list, (maxLen <<= 1) * sizeof(char *));
+		if (i + 2 > maxLen) {
+			maxLen = maxLen << 1;
+			list = realloc(list, maxLen * sizeof(char *));
+		}
 		list[i++] = getWord(&lastCh);
+		if (lastCh == 0)
+			i--;
+	}
+	if (i == 0) {
+		freeList(list);
+		return NULL;
 	}
 	list[i] = 0;
 	return list;
 }
 
+char isExit(char *word)
+{
+	return !strncmp(word, "exit", 5) || !strncmp(word, "quit", 5);
+}
+
+
 int main(void)
 {
+	char **command;
+
+	while (1) {
+		command = getList();
+		if (command == NULL) {
+			continue;
+		}
+		if (isExit(command[0])) {
+			freeList(command);
+			break;
+		}
+		if (fork() > 0) {
+			wait(NULL);
+		} else {
+			if (execvp(command[0], command) < 0) {
+				perror("exec failed");
+				return 1;
+			}
+		}
+		freeList(command);
+	}
 	return 0;
 }
