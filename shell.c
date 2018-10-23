49b758973dd6f1204a3b1a25f0f7d13c0b2dc86c
LocrianGoose <azairLocr@protonmail.ch>
2018-10-23 21:13:14 +0600

    stage3

--- shell.c
+++ shell.c
@@ -3,9 +3,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <string.h>
+
+typedef struct {
+	char instruction;
+	char *file;
+} SpecialCase;
 
 char *getWord(char *lastCh)
 {
@@ -13,21 +19,22 @@ char *getWord(char *lastCh)
 	char *word = malloc(maxLen * sizeof(char));
 	char buf;
 	int i = 0;
+	*lastCh = 0;
 
-	while ((buf = getchar()) != ' ' && buf != '\n') {
+	while ((buf = getchar()) != ' ' && buf != '\n' &&
+				buf != '>' && buf != '<') {
 		if (i + 1 >= maxLen) {
 			maxLen <<= 1;
 			word = realloc(word, maxLen * sizeof(char));
 		}
 		word[i++] = buf;
 	}
+	*lastCh = buf;
 	if (i == 0) {
-		*lastCh = 0;
 		free(word);
 		return NULL;
 	}
 	word[i] = 0;
-	*lastCh = buf;
 	return word;
 }
 
@@ -39,7 +46,7 @@ void freeList(char **command)
 	free(command);
 }
 
-char **getList(void)
+char **getList(SpecialCase *special)
 {
 	int maxLen = 2, i = 0;
 	char lastCh = ' ';
@@ -50,9 +57,23 @@ char **getList(void)
 			maxLen = maxLen << 1;
 			list = realloc(list, maxLen * sizeof(char *));
 		}
-		list[i++] = getWord(&lastCh);
-		if (lastCh == 0)
-			i--;
+		list[i] = getWord(&lastCh);
+		if (list[i] != NULL)
+			i++;
+		switch (lastCh) {
+		case '>':
+			special->instruction = '>';
+			do {
+				special->file = getWord(&lastCh);
+			} while (lastCh == ' ');
+			break;
+		case '<':
+			special->instruction = '<';
+			do {
+				special->file = getWord(&lastCh);
+			} while (lastCh == ' ');
+			break;
+		}
 	}
 	if (i == 0) {
 		freeList(list);
@@ -67,20 +88,12 @@ char isExit(char *word)
 	return !strncmp(word, "exit", 5) || !strncmp(word, "quit", 5);
 }
 
-
-int main(void)
+int sendCmd(char **command, SpecialCase *special)
 {
-	char **command;
+	int fd[2];
 
-	while (1) {
-		command = getList();
-		if (command == NULL) {
-			continue;
-		}
-		if (isExit(command[0])) {
-			freeList(command);
-			break;
-		}
+	switch (special->instruction) {
+	case 0:
 		if (fork() > 0) {
 			wait(NULL);
 		} else {
@@ -89,7 +102,63 @@ int main(void)
 				return 1;
 			}
 		}
+		break;
+	case '>':
+		fd[1] = open(special->file,
+				O_WRONLY | O_CREAT | O_TRUNC,
+				S_IRUSR | S_IWUSR);
+		free(special->file);
+		if (fork() > 0) {
+			wait(NULL);
+			dup2(1, fd[1]);
+		} else {
+			dup2(fd[1], 1);
+			if (execvp(command[0], command) < 0) {
+				perror("exec failed");
+				return 1;
+			}
+		}
+		close(fd[1]);
+		break;
+	case '<':
+		fd[0] = open(special->file, O_RDONLY, S_IRUSR);
+		free(special->file);
+		if (fork() > 0) {
+			wait(NULL);
+			dup2(0, fd[0]);
+		} else {
+			dup2(fd[0], 0);
+			if (execvp(command[0], command) < 0) {
+				perror("exec failed");
+				return 1;
+			}
+		}
+		close(fd[0]);
+		break;
+
+	}
+	return 0;
+}
+
+
+int main(void)
+{
+	char **command;
+	SpecialCase *special = malloc(sizeof(SpecialCase));
+
+	while (1) {
+		special->instruction = 0;
+		command = getList(special);
+		if (command == NULL) {
+			continue;
+		}
+		if (isExit(command[0])) {
+			freeList(command);
+			break;
+		}
+		sendCmd(command, special);
 		freeList(command);
 	}
+	free(special);
 	return 0;
 }
