f9ba6181f23152eee254f76a1f746c8ab2697a96
LocrianGoose <azairLocr@protonmail.ch>
2018-10-23 22:57:27 +0600

    stage4

--- shell.c
+++ shell.c
@@ -22,7 +22,7 @@ char *getWord(char *lastCh)
 	*lastCh = 0;
 
 	while ((buf = getchar()) != ' ' && buf != '\n' &&
-				buf != '>' && buf != '<') {
+				buf != '>' && buf != '<' && buf != '|') {
 		if (i + 1 >= maxLen) {
 			maxLen <<= 1;
 			word = realloc(word, maxLen * sizeof(char));
@@ -48,7 +48,7 @@ void freeList(char **command)
 
 char **getList(SpecialCase *special)
 {
-	int maxLen = 2, i = 0;
+	int maxLen = 4, i = 0;
 	char lastCh = ' ';
 	char **list = malloc(maxLen * sizeof(char *));
 
@@ -65,13 +65,16 @@ char **getList(SpecialCase *special)
 			special->instruction = '>';
 			do {
 				special->file = getWord(&lastCh);
-			} while (lastCh == ' ');
+			} while (special->file == NULL);
 			break;
 		case '<':
 			special->instruction = '<';
 			do {
 				special->file = getWord(&lastCh);
-			} while (lastCh == ' ');
+			} while (special->file == NULL);
+			break;
+		case '|':
+			special->instruction = '|'
 			break;
 		}
 	}
@@ -134,6 +137,21 @@ int sendCmd(char **command, SpecialCase *special)
 			}
 		}
 		close(fd[0]);
+		break;
+	case '|':
+		pipe(fd);
+		if (fork() > 0) {
+			wait(NULL);
+		} else {
+			close(fd[0]);
+			dup2(fd[1], 1);
+			if (execvp(command[0], command) < 0) {
+				perror("exec failed");
+				return 1;
+			}
+		}
+		close(fd[1]);
+		dup(fd[0], 0);
 		break;
 
 	}
