a9a37cbec9d16a14f490078086f396cb3fa32b8c
LocrianGoose <azairLocr@protonmail.ch>
2018-11-08 19:58:51 +0600

    splited some frequent code into functions and removed redundant close()

--- shell.c
+++ shell.c
@@ -18,6 +18,33 @@
 #define PIPE_CODE -23
 #define AMPERSAND -42
 
+
+int superClose(int fd)
+{
+	if (fd != 1 && fd != 0)
+		if (close(fd)) {
+			perror("close failed");
+			exit(1);
+		}
+	return 0;
+}
+
+void *superRealloc(void *ptr, int size)
+{
+	ptr = realloc(ptr, size);
+	if (ptr == NULL) {
+		perror("realloc failed");
+		exit(1);
+	}
+	return ptr;
+}
+
+void superDup2(int oldfd, int newfd)
+{
+	dup2(oldfd, newfd);
+	superClose(oldfd);
+}
+
 char *getWord(char *lastCh)
 {
 	int maxLen = 1;
@@ -31,11 +58,7 @@ char *getWord(char *lastCh)
 				buf != '|' && buf != '&') {
 		if (i + 1 >= maxLen) {
 			maxLen <<= 1;
-			word = realloc(word, maxLen * sizeof(char));
-			if (word == NULL) {
-				perror("realloc failed");
-				exit(1);
-			}
+			word = superRealloc(word, maxLen * sizeof(char));
 		}
 		word[i++] = buf;
 	}
@@ -72,11 +95,7 @@ char **getList(int *fd, char *lastCh)
 	while (*lastCh != '\n' && *lastCh != '|' && *lastCh != '&') {
 		if (i + 1 >= maxLen) {
 			maxLen = maxLen << 1;
-			list = realloc(list, maxLen * sizeof(char *));
-			if (list == NULL) {
-				perror("realloc failed");
-				exit(1);
-			}
+			list = superRealloc(list, maxLen * sizeof(char *));
 		}
 		list[i] = getWord(lastCh);
 		if (list[i] != NULL)
@@ -86,8 +105,7 @@ char **getList(int *fd, char *lastCh)
 			do {
 				fileName = getWord(lastCh);
 			} while (fileName == NULL);
-			if (fd[1] != 1)
-				close(fd[1]);
+			superClose(fd[1]);
 			fd[1] = open(fileName,
 				O_RDWR | O_CREAT | O_TRUNC, 0666);
 			free(fileName);
@@ -96,8 +114,7 @@ char **getList(int *fd, char *lastCh)
 			do {
 				fileName = getWord(lastCh);
 			} while (fileName == NULL);
-			if (fd[0] != 0)
-				close(fd[0]);
+			superClose(fd[0]);
 			fd[0] = open(fileName, O_RDONLY);
 			free(fileName);
 			break;
@@ -135,17 +152,9 @@ char ***getSuperList(int (**fd)[3], int *length)
 		lastChar = ' ';
 		if (i + 1 >= maxLen) {
 			maxLen = maxLen << 1;
-			superList = realloc(superList,
+			superList = superRealloc(superList,
 					maxLen * sizeof(char **));
-			if (superList == NULL) {
-				perror("realloc failed");
-				exit(1);
-			}
-			*fd = realloc(*fd, maxLen * sizeof(int[3]));
-			if (*fd == NULL) {
-				perror("realloc failed");
-				exit(1);
-			}
+			*fd = superRealloc(*fd, maxLen * sizeof(int[3]));
 		}
 		superList[i] = getList((*fd)[i], &lastChar);
 		if (superList[i] != NULL)
@@ -181,24 +190,17 @@ int sendCmd(char **command, int *fd, int closeNext)
 		perror("fork failed");
 		exit(1);
 	} else if (pid == 0) {
-		if (closeNext != 0)
-			close(closeNext);
-		dup2(fd[0], 0);
-		dup2(fd[1], 1);
-		if (fd[1] != 1)
-			close(fd[1]);
-		if (fd[0] != 0)
-			close(fd[0]);
+		superClose(closeNext);
+		superDup2(fd[0], 0);
+		superDup2(fd[1], 1);
 		if (execvp(command[0], command) < 0) {
 			perror("exec failed");
 			puts(command[0]);
 			exit(1);
 		}
 	}
-	if (fd[1] != 1)
-		close(fd[1]);
-	if (fd[0] != 0)
-		close(fd[0]);
+	superClose(fd[1]);
+	superClose(fd[0]);
 	return pid;
 }
 
@@ -223,10 +225,6 @@ int superSendCmd(char ***command, int (*fd)[3], int length)
 		if (fd[i][0] != 0)
 			superfd[0] = fd[i][0];
 		sendCmd(command[i], superfd, closeNext);
-		if (superfd[0] != 0)
-			close(superfd[0]);
-		if (superfd[1] != 1)
-			close(superfd[1]);
 		if (!fd[i][2])
 			wait(NULL);
 	}
